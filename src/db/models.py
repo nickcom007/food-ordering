e8f40e3e69087cd3096157c9ef3695038b1784a4
Yizhe Wen <yizhewen@Yizhes-MacBook-Pro.local>
2021-11-19 19:49:33 -0500

    db

---
+++ src/db/models.py
@@ -0,0 +1,60 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+class Customer(Base):
+
+    __tablename__ = "customer"
+
+    email = Column(String(32), primary_key=True, index=True)
+    first_name = Column(String(24))
+    last_name = Column(String(24))
+    hashed_password = Column(String(32))
+    enabled = Column(Boolean, default=True)
+
+    cart = relationship("Cart", back_populates="customer")
+
+
+class Cart(Base):
+
+    __tablename__ = "cart"
+
+    id = Column(Integer, primary_key=True, index=True)
+    total_price = Column(Float(32), default=0, nullable=False)
+    customer_email = Column(String(32), ForeignKey("customer.email"))
+
+    customer = relationship("Customer", back_populates="cart")
+
+
+class OrderItem(Base):
+
+    __tablename__ = "orderitem"
+
+    id = Column(Integer, primary_key=True, index=True)
+    quantity = Column(Integer)
+    price = Column(Float(32))
+    menu_item_id = Column(ForeignKey("menuitem.id"))
+    cart_id = Column(ForeignKey("cart.id"))
+
+
+class MenuItem(Base):
+
+    __tablename__ = "menuitem"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(24))
+    price = Column(Float(32))
+    description = Column(String(64))
+    restaurant_id = Column(ForeignKey("restaurant.id"))
+
+
+class Restaurant(Base):
+
+    __tablename__ = "restaurant"
+
+    id = Column(Integer, primary_key=True, index=True)
+    address = Column(String(64))
+    name = Column(String(32))
+    phone = Column(String(12))
+    image_url = Column(String(512))
